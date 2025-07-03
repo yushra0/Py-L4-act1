4e3b5c8e1302d3f13d30c62ecda298b897723277
yushra0 <zayaanyushra@gmail.com>
2025-07-03 15:36:44 +0600

    Add files via upload

---
+++ py L4 act1.py
@@ -0,0 +1,5 @@
+# Python program to print the multiplication table of 23 from 1 to 10
+# Loop from 1 to 10
+for i in range(1, 11):
+    # Print the multiplication of 23 by i
+    print(f"23 x {i} = {23 * i}")
